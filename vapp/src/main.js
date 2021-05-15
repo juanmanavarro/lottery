fcf84996c6310071eac29ea5403da501b0d93966
Juanma <juanaco1.p@gmail.com>
2021-05-15 12:52:14 +0200

    dark style

--- vapp/src/main.js
+++ vapp/src/main.js
@@ -1,15 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
 import store from "./store/index.js";
-import router from "./router.js";
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
-import Toasted from 'vue-toasted';
 
-Vue.use(BootstrapVue);
-Vue.use(IconsPlugin);
-
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/dark.min.css'
 
 // Todo: Update this when publishing
 // import drizzleVuePlugin from '@drizzle/vue-plugin'
@@ -19,12 +12,9 @@ import drizzleOptions from './drizzleOptions'
 
 Vue.use(drizzleVuePlugin, { store, drizzleOptions })
 
-Vue.use(Toasted);
-
 Vue.config.productionTip = false
 
 new Vue({
   store,
-  router,
   render: h => h(App)
 }).$mount('#app')
